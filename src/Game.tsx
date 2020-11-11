99a18d666360113cae0fe94999e70d76fee3d9f9
Davide Pedron <davide@happydev.cloud>
2020-11-11 02:21:53 +0100

    Remove commented code

--- src/Game.tsx
+++ src/Game.tsx
@@ -68,35 +68,6 @@ function Game() {
     case Locations.NotKnown:
       return (<></>)
   }
-
-  // return (
-  //   <>
-  //
-  //     <div id="chat-page" className="hidden">
-  //       <div className="chat-container">
-  //         <div className="chat-header">
-  //           <h2>Welcome to Spring Boot Chat Application</h2>
-  //         </div>
-  //         <div className="connecting">
-  //           Connecting...
-  //         </div>
-  //         <ul id="messageArea">
-  //
-  //         </ul>
-  //         <form id="messageForm" name="messageForm" onSubmit={sendMessage}>
-  //           <div className="form-group">
-  //             <div className="input-group clearfix">
-  //               <input type="text" id="message" placeholder="Type a message..." autoComplete="off"
-  //                      className="form-control"/>
-  //               <button type="submit" className="primary">Send</button>
-  //             </div>
-  //           </div>
-  //         </form>
-  //       </div>
-  //     </div>
-  //
-  //   </>
-  // );
 }
 
 export default Game;
